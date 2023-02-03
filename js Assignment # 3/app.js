0f2067a0beee85b248e5165e4fe66242fdf65622
SyedMudassir1 <122387591+SyedMudassir1@users.noreply.github.com>
2023-02-03 07:06:48 -0800

    Add files via upload

---
+++ js Assignment # 3/app.js
@@ -0,0 +1,223 @@
+
+//************* chapter # 5 ************//
+
+//Question no 1
+
+ // var num1 = 3;
+ // var num2 = 5;
+ //num1 + num2;
+ // console.log(num1 + num2);
+ // document.write("sum of 3 and 5 is 8");
+
+ //Question no 2
+ 
+ // == subtraction ==
+ // var sub1 = 3;
+ // var sub2 = 5;
+ // num1 - num2;
+ // console.log(sub1 - sub2);
+
+ // == multiplication ==
+ // var mlt1 = 3;
+ // var mlt2 = 5;
+ // num1 * num2;
+ // console.log(mlt1 * mlt2);
+
+ // *** division ***
+ // var div1 = 3;
+ // var div2 = 5;
+ // num1 / num2;
+ // console.log(div1 / div2);
+
+ // == modulus ==
+ // var x = 3;
+ // var y = 5;
+ // x % y;
+ // console.log(x % y);
+
+ //Question no 3
+ // (a)
+ // var num = ;
+ //  document.write("value after variable declaration is undefiend");
+ // var num1 = 5;
+ // document.wright("initial value: 5");
+ //   (b)
+ // var num1 = 5;
+ // var num2 = 1;
+ // num1 + num2;
+ // console.log(num1 + num2);
+ // document.write("vale after increment is: 6");
+ // (c)
+ // var a = 6;
+ // var b = 7;
+ // a + b;
+ // document.write("value after addtion is: 13");
+ // (d)
+  //var a = 6;
+ // var b = 7;
+  // cosole.log(a6 +b7 -1 );
+ // document.write("value after decrement is: 12");
+ // (e) (f)
+ // var a = 6;
+ // var b = 7;
+ // a - b = 0;
+ // document.write("the remainder is: 0");
+
+ //Question no 4
+
+ // var ticket1 = 600;
+ // var ticket2 = 5;
+ // ticket1 * ticket2;
+ // console.log(ticket1 * ticket2);
+ // document.write("total cost to buy 5 tickets to movies is 3000PKR");
+
+ //Question no 5
+
+ // document.write("Table of 4 <br> 4×1= 4 <br> 4×2= 8 <br> 4×3= 12 <br> 4×4= 16 <br> 4×5= 20 <br> 4×5= 24 <br> 4×7= 28 <br> 4×8= 32 <br> 4×9= 36 <br> 4×10= 40");
+// document.write("<h2> TABLE OF 4 </h2>");
+// document.write("<h2> 4 * 1 = 4 </h2>");
+// document.write("<h2> 4 * 2 = 8 </h2>");
+// document.write("<h2> 4 * 3 = 12 </h2>");
+// document.write("<h2> 4 * 4 = 16 </h2>");
+// document.write("<h2> 4 * 5 = 20 </h2>");
+// document.write("<h2> 4 * 6 = 24 </h2>");
+// document.write("<h2> 4 * 7 = 28 </h2>");
+// document.write("<h2> 4 * 8 = 32 </h2>");
+// document.write("<h2> 4 * 9 = 36 </h2>");
+// document.write("<h2> 4 * 10 = 40 </h2>");
+
+
+//Question no 6
+//(a)
+//var temp = 30c;
+//(b)
+// var temp = 30;
+// temp = (30 * 5 / 9) + 32;
+// alert(temp);
+// document.write("<h2> 30 celcius into fahrenheit is 48.66666666666667 </h2>");
+//(c)
+// var temp = 300;
+//(d)
+// var temp = 300;
+// temp = (300 - 32) * 5 / 9;
+// alert(temp);
+// document.write("<h2> 300 fah into celcius is 148.8888888888888889</h2>")
+
+
+ //Question no 7
+
+ // var item1 = 650;
+ // var quanitityItem = 3;
+ // item1 * quanitityItem;
+ //  var total1 = 1950;
+ // var item2 = 1000;
+ // var quanitity2 = 7;
+ // item2 * quanitity2;
+ // var total2 = 700;
+ // var ship = 100;
+ // total1 + total2 + ship;
+ // tota = 2750; 
+ // document.write("<h1>Shopping Cart</h1> <br> price of item1 is 650 <br> price quantity of item 1 is 3 <br> price of item2 is 100 <br> quanitity of item 2 is 7 <br> shipping charges 100 <br> <br> Total cost of your order is 2750");
+ 
+//Question no 8 
+// var totalMarks = 980;
+// var obtained = 804;
+// var percentages = 100;
+// obtained / totalMarks * 100;
+// document.write("<h1>Marks Sheet</h1> <br> Total marks:980 <br> Marks obtained:804 <br> percentage: 82.0408163265306%");
+
+//Question no 9
+
+// 10$ * 104.80 = 1048 , 25 * 28 = 700,
+// var pkr = 1748;
+// pkr = 1748 / 262;
+// alert(pkr);
+//document.write("<h2>Pkr in doller: 6.671756</h2>");
+
+// document.write("<h1>Currency in PKR</h1> <br> <br>total currency in pkr:1748");
+
+//Question no 10
+
+// var num1 = 5;
+// num1 = + 5;
+// num1 = + 5 * 1o / 2;
+// alert("30");
+
+//Question no 11
+// var currentYear = 2016;
+// var birth = 1992;
+// currentYear = 2016 - 1992;
+// var result = 24;
+// document.write("<h1>Age Calculator</h2> <br> <br> Current Year:2016 <br> Birth Year:1992 <br> Your Age is: 24"); 
+
+//Question no 12
+
+// document.write("<h1>The Geometrizer</h1>");
+// (a)
+// var radius = 30;
+// document.write("<h2><i>Radius of a circle is: 30</i></h2>");
+//(b)
+// var radius = 30;
+// radius = 3.143 * 30 * 30;
+// alert(radius);
+// document.write("<h2>Area of a circle is: 2828.7</h2>");
+//(c)
+// radius = 2 * 3.143 * 30;
+// alert(radius);
+// document.write("<h2>Circimference of a circle is: 188.5799999</h2>")
+
+
+//Question no 13
+// var favouriteSnack = chocolate chip;
+// var age = 15;
+// varestimatedMaximumAges = 65;
+// snack = 3 * 65 - 45;
+// alert("150");
+// document.write("<h1>The Lifetime Supply Caleculator</h1> <br> <br> Favourite Snack: chocolate chip <br> Current age: 15 <br> Estimated Maximum Age:65 <br> Amount of snacks per day: 3 <br> You will need 150 chocolate chip to last you until the ripe old age of 65");
+
+// ********** THE END ***********//
+
+// ****** chapter # 9 ****** //
+
+//Question no 1 
+
+// var valueA = 10;
+// valueA = valueA ++ + ++ - -- - --;
+// alert(" The value of a is: 10");
+// document.write("Result: <br> The value of a is: 10 <br> ..................................... <br> <br> The value of ++a is: 11 <br> Now the value of a is: 11 <br> <br> The value of a++ is: 11 <br> Now the value of a is 12: <br> <br> The value of --a is: 11 <br> Now the value of a is: 11 <br> <br> The value of a-- is: 11 <br> Now the value of a is: 10");
+
+//Question no 2
+
+// var a = 2;
+// var b = 1;
+// a + b = -aa - --b + ++b + b-- = 3;
+// alert("3");
+// --a; 1
+// --a - --b; 0
+// --a - --b + ++b; 1 
+// --a - --b + ++b + b--; 1
+// document.write("a is: 2 <br> b is: 1 <br> result is: 3");
+
+//Question no 3 
+// alert("Hi");
+
+//Question no 4
+
+//Question no 5 
+
+// alert("03126574670");
+// document.write("<table><ul><li>o3126574670</li><li>o3126574670</li><li>03126574670</li></ul></table>");
+
+//Question no 6
+ 
+// varA = 54;
+// varB = 54;
+// var totalMarks = 100;
+// alert("54");
+// alert("54");
+// var totalObtainedMarks = 156;
+// document.write("<h1>Subject Total Marks Obtained Marks Percentage</h1> <br> <br> <table> <ul> <th> <tr>English</tr> <tr> 100 <tr> 54 </tr><tr> 54% </tr></tr> <br><tr><br> Math</tr><tr> 100 </tr> <tr> 54 </tr> <tr> 54% </tr> <br><br> <tr>Urdu</tr> <tr></tr> 100  <tr> 54 </tr> <tr> 48% </tr></th> <br> <br> <tr> 300 </tr> <tr> 156 </tr> <tr> 52% </tr> </tr>")
+
+//****************************** THE END *********************************//
+
+
